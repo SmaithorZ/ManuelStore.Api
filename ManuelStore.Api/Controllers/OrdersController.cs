d521952357dddd6aaee772a809169d5ecbc290a8
Mateus Araújo <smaithorz@gmail.com>
2025-05-28 13:01:51 -0300

    Modified ProcessOrders Action to receive a list of orders, and other stuff.

--- ManuelStore.Api/Controllers/OrdersController.cs
+++ ManuelStore.Api/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
-﻿using ManuelStore.Api.Models;
+﻿using System.Linq.Expressions;
+using ManuelStore.Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,21 +10,63 @@ namespace ManuelStore.Api.Controllers
     public class OrdersController : ControllerBase
     {
 
-        [HttpPost]
+        [HttpPost("ProcessOrders")]
         public IActionResult ProcessOrders([FromBody] List<Order> orders)
         {
-            var response = orders.Select(o => new OrderResponse
-            {
-               OrderId = o.OrderId,
-               Boxes = PackProductsInBoxes(o.Products)
+            var response = new List<OrderResponse>();
 
-            }).ToList();
+            foreach (var order in orders)
+            {
+                var boxResponses = new List<BoxResponse>();
+
+                foreach (var product in order.Products)
+                {
+                    var fittingBox = AvailableBoxes.Boxes.FirstOrDefault(box => ProductFitsBox(product, box));
+
+                    if (fittingBox == null)
+                    {
+                        //Product does not fi in  any box
+                        boxResponses.Add(new BoxResponse
+                        {
+                            BoxId = null,
+                            Products = new List<string> { product.ProductId },
+                            Note = "Product does not fit in any available box."
+                        });
+                    }
+                    else
+                    {
+                        //Verifies if this box already exists in the list
+                        var existingBoxResponse = boxResponses.FirstOrDefault(br => br.BoxId == fittingBox.BoxId);
+
+                        if (existingBoxResponse != null)
+                        {
+                            existingBoxResponse.Products.Add(product.ProductId);
+                        }
+                        else
+                        {
+                            boxResponses.Add(new BoxResponse
+                            {
+                                BoxId = fittingBox.BoxId,
+                                Products = new List<string> { product.ProductId },
+                                Note = null
+                            });
+                        }
+                    }
+                }
+
+                response.Add(new OrderResponse
+                {
+                    OrderId = order.OrderId,
+                    Boxes = boxResponses
+                });
+            }
 
             return Ok(new { orders = response });
         }
-        
+
 
         private List<BoxResponse> PackProductsInBoxes(List<Product> products)
+
         {
             var packedBoxes = new List<BoxResponse>();
 
@@ -55,6 +98,14 @@ namespace ManuelStore.Api.Controllers
 
             return packedBoxes; 
         } 
+
+
+        private bool ProductFitsBox(Product product, Box box)
+        {
+            return product.Dimensions.Height <= box.Height &&
+                   product.Dimensions.Width <= box.Width &&
+                   product.Dimensions.Length <= box.Length;
+        }
 
     }
 }
